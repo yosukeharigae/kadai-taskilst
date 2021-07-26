9e1afbd1fe566549a4cbc752e3a5f0b867761b91
yosuke.harigae <xoxo18594@gamil.com>
2021-07-26 23:40:42 +0900

    Add IndexServlet.java

--- src/task/Task.java
+++ src/task/Task.java
@@ -7,10 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="task")
+@NamedQueries({
+	@NamedQuery(
+			name="getAlltasks",
+			query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+		    )
+})
+@Table(name="tasks")
 public class Task {
 
 	@Id
