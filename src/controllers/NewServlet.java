0530b43e50baf303b566581fadf69a8a0ecbea43
yosuke.harigae <xoxo18594@gamil.com>
2021-07-27 04:48:53 +0900

    Add NewServlet.java

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,11 +1,17 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import task.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -13,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,8 +32,25 @@ public class NewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
+
+		Task t =new Task();
+
+		String content="hello";
+		t.setContent(content);
+
+		Timestamp currenTime= new Timestamp(System.currentTimeMillis());
+		 t.setCreated_at(currenTime);
+	        t.setUpdated_at(currenTime);
+
+	        em.getTransaction().begin();
+	        em.persist(t);
+	        em.getTransaction().commit();
+
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append(Integer.valueOf(t.getId()).toString());
+
+				em.close();
 	}
 
 }
